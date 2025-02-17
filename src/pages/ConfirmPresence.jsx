f0e4bc27e2b227c70714b2e7c60a53ae29fb42d9
hallan-n <hallanneves01@gmail.com>
2025-02-17 18:45:18 -0300

    Adicionado a funcionalidade de incluir filhos

--- src/pages/ConfirmPresence.jsx
+++ src/pages/ConfirmPresence.jsx
@@ -3,6 +3,11 @@ import { useState } from "react";
 
 export default function ConfirmPresence() {
     const [isOpen, setIsOpen] = useState(false);
+    const [inputs, setInputs] = useState([""]);
+
+    const addInput = () => {
+        setInputs([...inputs, ""]);
+    };
 
     return (
         <div>
@@ -27,31 +32,38 @@ export default function ConfirmPresence() {
                         className="mt-2 w-full rounded-lg border border-gray-300 bg-white py-2 p-4 text-gray-900 shadow-sm focus:outline-none focus:ring-2" />
                     <div className='hidden'>
                         <h2 className='font-bold mt-2'>Seus dados</h2>
-                    <input
-                        type="tel"
-                        placeholder="Celular"
-                        className="mt-2 w-full rounded-lg border border-gray-300 bg-white py-2 p-4 text-gray-900 shadow-sm focus:outline-none focus:ring-2" />
+                        <input
+                            type="tel"
+                            placeholder="Celular"
+                            className="mt-2 w-full rounded-lg border border-gray-300 bg-white py-2 p-4 text-gray-900 shadow-sm focus:outline-none focus:ring-2" />
 
                     </div>
 
 
 
-                    <div className='mt-4 transition rounded-md text-white w-full'>
+                    <div className='mt-4 mb-1 transition rounded-md text-white w-full bg-zinc-100'>
                         <button
                             onClick={() => setIsOpen(!isOpen)}
-                            className="w-full p-2 bg-zinc-500 hover:bg-zinc-600 rounded-md cursor-pointer"
-                        >Adicionar acompanhante
+                            className="w-full flex justify-between py-2 px-5 bg-zinc-500 hover:bg-zinc-600 rounded-md cursor-pointer"
+                        >
+                            <span>{isOpen ? "▲" : "▼"}</span>
+                            <p>Incluir filhos</p>
+                            
                         </button>
                         {isOpen && (
-                            <div className="mt-2 mb-6 text-black">
-                                <input
-                                    type="text"
-                                    placeholder="Acompanhante"
-                                    className="mt-2 w-full rounded-lg border border-gray-300 bg-white py-2 p-4 text-gray-900 shadow-sm focus:outline-none focus:ring-2" />
-                                     <textarea
-                                     placeholder='Nome dos filhos'
-                                        className="mt-2 w-full rounded-lg border border-gray-300 bg-white py-2 p-4 text-gray-900 shadow-sm focus:outline-none focus:ring-2"
-                                      name="" id=""></textarea>
+                            <div className='flex gap-4 justify-between my-5 pb-5 mx-4'>
+                                <div>
+                                    {inputs.map((_, index) => (
+                                        <input
+                                            key={index}
+                                            type="text"
+                                            placeholder={`Nome o filho ${index + 1}`}
+                                            className="mt-2 w-full rounded-lg border border-gray-300 bg-white py-2 p-4 text-gray-900 shadow-sm focus:outline-none focus:ring-2" />
+                                    ))}
+                                </div>
+                                <button onClick={addInput} className="self-end p-3 pb-2 bg-zinc-500 hover:bg-zinc-600 cursor-pointer rounded-full">
+                                    <span class="material-symbols-outlined m-0 p-0 text-white">add</span>
+                                </button>
                             </div>
                         )}
                     </div>
