0b529e142a55d5c2f24384b585bd0dccc32f48f1
hallan-n <hallanneves01@gmail.com>
2025-02-26 13:43:19 -0300

    Adicionado filtro de confirmados

--- src/components/InvitationList.jsx
+++ src/components/InvitationList.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom'
 export default function InvitationList() {
     const [searchTerm, setSearchTerm] = useState('')
     const [convidados, setConvidados] = useState([])
+    const [confirmed, setConfirmed] = useState(null)
 
     useEffect(() => {
         const fetchConvidados = async () => {
@@ -15,20 +16,21 @@ export default function InvitationList() {
                 console.error('Erro ao buscar convidados:', error)
             }
         }
-
         fetchConvidados()
     }, [])
 
     const filteredConvidados = convidados.filter(guest =>
-        guest.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-        guest.email.toLowerCase().includes(searchTerm.toLowerCase())
+        (guest.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+            guest.email.toLowerCase().includes(searchTerm.toLowerCase())) &&
+        (confirmed === null || guest.is_confirmed === confirmed)
     )
 
     return (
         <div className="p-4">
             <h1 className='text-2xl font-bold mb-5'>Gerenciador de lista de convidados</h1>
-            <h2>Filtros</h2>
-            <div className='flex'>
+            <h2 className='font-bold'>Filtros</h2>
+            <hr className='border-zinc-200 mt-2 mb-5'/>
+            <div className='flex gap-4'>
                 <div >
                     <p>Pesquisa por nome</p>
                     <input
@@ -41,7 +43,8 @@ export default function InvitationList() {
                 </div>
                 <div>
                     <p>Confirmou presença</p>
-                    <select name="" id="">
+                    <select onChange={(e) => setConfirmed(e.target.value === "1" ? true : e.target.value === "0" ? false : null)} className="mb-4 p-2 border border-gray-300 rounded">
+                        <option value="" selected>Todos</option>
                         <option value="1">Sim</option>
                         <option value="0">Não</option>
                     </select>
@@ -63,9 +66,11 @@ export default function InvitationList() {
                             <td className="p-4">{guest.name}</td>
                             <td className="p-4">{guest.email}</td>
                             <td className="p-4">
-                                <Link to="/gift-list">
-                                    <span className="material-symbols-outlined">edit</span>
-                                    <span className="material-symbols-outlined">delete</span>
+                                <Link to={`/admin-panel/edit-guest?id=${guest.id}`}>
+                                    <span className="material-symbols-outlined">edit</span>                                
+                                </Link>
+                                <Link to={`/admin-panel/confirm-delete-guest?id=${guest.id}`}>
+                                    <span className="material-symbols-outlined">delete</span>                                
                                 </Link>
                             </td>
                         </tr>
