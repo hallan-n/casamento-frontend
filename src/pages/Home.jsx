00fa5df3300a5112892fabc3fac63649537963b6
hallan-n <hallanneves01@gmail.com>
2025-03-17 23:31:08 -0300

    Replace multiple image assets with new ones; add CaviarDreams font files and update styles for font integration in Home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,26 @@
 import React from 'react'
 import Header from '../components/Header'
-import canto from '../assets/canto.png'
-import flores from '../assets/flores.png'
-import teste from '../assets/teste.png'
+import casal from '../assets/casal.png'
+import divider from '../assets/divider.svg'
+import logo_full from '../assets/logo_full.svg'
 
 export default function Home() {
   return (
-    <div className=''>
+    <div>
       <Header />
-      <main className='flex justify-center items-center bg-olive-2 relative h-screen' style={{ background: "radial-gradient(circle, rgba(103, 114, 84), rgba(57, 64, 48))" }}>
-        <img src={canto} alt="Imagem" class="absolute bottom-0 left-0 w-60 h-60" />
-
-        <div className='text-white p-4 max-w-4xl'>
-          <div className='flex items-center gap-5'>
-            <img src={teste} alt="Imagem" className='w-2xl' />
-            <div>
-              <h1 className='font-bold text-5xl mb-4'>Love Wedding</h1>
-              <p>simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's</p>
+      <main className="h-screen" style={{ background: "radial-gradient(circle, rgba(103, 114, 84), rgba(57, 64, 48))" }}>
+        <img className='hidden lg:block absolute top-0 left-0 h-screen w-auto' src={casal} alt="Casal" />
+        <div className='max-w-6xl mx-auto'>
+          <div className='flex justify-center lg:justify-end h-screen'>
+            <div className='text-white text-center flex flex-col justify-center items-center max-w-2xl p-4'>
+              <img class="w-96" src={logo_full} alt="Casal" />
+              <img class="w-96 my-8" src={divider} alt="Casal" />
+              <p className='font-caviar font-bold text-6xl'>Save the Date</p>
+              <p className='font-caviar font-bold mt-8 text-2xl'>Sábado, 04 de Outubro!</p>
+              <p className='font-caviar text-2xl mt-2'>Gostariamos de convidá-lo(a)</p>
+              <p className='font-caviar text-2xl mt-2'>para celebrar conosco este</p>
+              <p className='font-caviar text-2xl mt-2'>momento especial de união.</p>
             </div>
-          </div>
-          <div className='mx-auto mb-4 flex gap-4 justify-between absolute bottom-16'>
-            <div className='w-40 h-72 rounded-md bg-white'></div>
-            <div className='w-40 h-72 rounded-md bg-white'></div>
-            <div className='w-40 h-72 rounded-md bg-white'></div>
-            <div className='w-40 h-72 rounded-md bg-white'></div>
           </div>
         </div>
       </main>
